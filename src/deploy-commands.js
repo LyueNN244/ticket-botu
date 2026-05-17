a07cdf9bc5c3a7e8ee0b38d16521958858c5f39b
LyueNN244 <lolimus234@outlook.com>
2026-05-17 08:19:48 +0300

    Make bot global for multiple servers

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -11,10 +11,7 @@ try {
   console.log("Ticket komutları yükleniyor...");
 
   await rest.put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
+    Routes.applicationCommands(process.env.CLIENT_ID),
     { body: commands }
   );
 
